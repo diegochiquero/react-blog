08f6eb37980192573084079da2d8da94cdad8bf9
diegochiquero <chiquerodiego@gmail.com>
2022-03-23 16:47:10 +0100

    feat: add some styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+import Home from "./views/Home";
+
+import { Grid } from "@mui/material";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <div className="App">
+          <NavBar />
+          <div className="content">
+            <Home />
+          </div>
+        </div>
+      </Grid>
+    </Container>
   );
 }
 
