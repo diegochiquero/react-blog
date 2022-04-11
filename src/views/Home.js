28c673e7ec006678e676b3676eb2a1c9906e5980
diegochiquero <chiquerodiego@gmail.com>
2022-04-11 19:46:11 +0200

    feat: add useFetch

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,20 @@
-import { useState } from "react";
 import BlogList from "../components/BlogList";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "Breaking news", body: "lorem ipsum...", author: "Bruce", id: 1 },
-    { title: "Hello there!", body: "lorem ipsum...", author: "Hill", id: 2 },
-    { title: "Web fantasy", body: "lorem ipsum...", author: "Bruce", id: 3 },
-  ]);
+  const {
+    datas: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
   return (
     <div className="home">
-      {/* "props" sent from parent to child (blogs, title, handleDelete) */}
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {/* "props" sent from parent to child (blogs, title) */}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      {/* above code is javascript syntax that the reason why is between curly brackets*/}
     </div>
   );
 };
