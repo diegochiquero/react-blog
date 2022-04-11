a1b8d39daa47309291484d04fdae498db0f711c0
diegochiquero <chiquerodiego@gmail.com>
2022-04-11 19:45:26 +0200

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import NavBar from "./components/NavBar";
 import Home from "./views/Home";
+import Create from "./views/Create";
+import BlogDetails from "./views/BlogDetails";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
